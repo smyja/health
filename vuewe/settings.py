ceb3ff0531830ebaaff2020629235b1554d08b4f
smyja <akposlive59@gmail.com>
2020-07-22 18:23:23 +0100

    forgot password template

--- vuewe/settings.py
+++ vuewe/settings.py
@@ -123,10 +123,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS =[os.path.join(BASE_DIR, 'static')]
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = '587'
+EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'safiawatson2@gmail.com'
 EMAIL_HOST_PASSWORD ='Akpobi101##'
