31ef6de894a99fd31f61002280015a13951eed0e
smyja <akposlive59@gmail.com>
2020-08-18 15:03:21 +0100

    tu

--- vuewe/wsgi.py
+++ vuewe/wsgi.py
@@ -9,10 +9,10 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 
 import os
 import sys
-
+from django.core.wsgi import get_wsgi_application
 sys.path.insert(0, '/opt/python/current/app')
 
-from django.core.wsgi import get_wsgi_application
+
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vuewe.settings')
 
