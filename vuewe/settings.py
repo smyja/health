cb2f3802a9f6e0896fb47e3407ea99a4fb329fe0
smyja <akposlive59@gmail.com>
2020-08-22 08:48:43 +0100

    tu

--- vuewe/settings.py
+++ vuewe/settings.py
@@ -90,19 +90,22 @@ WSGI_APPLICATION = 'vuewe.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-         'TEST': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        },
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        #  'TEST': {
+        #     'ENGINE': 'django.db.backends.sqlite3',
+        #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql', 'NAME': 'NavaraDb', 'USER': 'postgres',
+            'PASSWORD': 'Akpobi101$', 'HOST': 'localhost', 'PORT': '5432',
+
+        
     
     }
 }
 
 
-if 'test' in sys.argv:
-    DATABASES['default'] = {'ENGINE': 'django.db.backends.sqlite3'}
+# if 'test' in sys.argv:
+#     DATABASES['default'] = {'ENGINE': 'django.db.backends.postgresql','NAME':'NavaraDb','USER':'postgres','PASSWORD':'Akpobi101$$','HOST':'localhost','PORT':'5432'}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
