8b2358c5d37a38402e727fba27a9c3cb2200ec83
smyja <akposlive59@gmail.com>
2020-11-26 09:28:08 +0100

    changed homepage

---
+++ vuewe/__init__.py
@@ -0,0 +1,12 @@
+# import os
+# import imp
+# import os 
+# import sys
+# from django_app.wsgi import application
+# sys.path(0, os.path.path.dirname(__file__))
+
+
+# sys.path.insert(0, os.path.dirname(__file__))
+
+# wsgi = imp.load_source('wsgi', 'passenger_wsgi.py')
+# application = wsgi.application
