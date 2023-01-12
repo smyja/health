c5d07c96caa62bdb87b5977befa1442b2a0d8a14
Smyja <akposlive59@gmail.com>
2023-01-12 22:31:34 +0100

    added storages

--- vuewe/settings.py
+++ vuewe/settings.py
@@ -169,3 +169,17 @@ DEFAULT_FROM_EMAIL = 'Navara <noreply@navarar.live>'
 SIMPLE_MAIL_USE_CKEDITOR = True
 SIMPLE_MAIL_USE_MODELTRANSLATION = True
 SIMPLE_MAIL_FILE_STORAGE = 'django.core.files.storage.FileSystemStorage'
+
+AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
+AWS_STORAGE_BUCKET_NAME = "clairevara"
+AWS_S3_ENDPOINT_URL = "https://nyc3.digitaloceanspaces.com"
+AWS_S3_OBJECT_PARAMETERS = {"CacheControl": "max-age=86400", "ACL": "public-read"}
+AWS_LOCATION = "static"
+
+STATIC_URL = f"https://{AWS_S3_ENDPOINT_URL}/{AWS_LOCATION}/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+PUBLIC_MEDIA_LOCATION = "media"
+MEDIA_URL = f"https://{AWS_S3_ENDPOINT_URL}/{PUBLIC_MEDIA_LOCATION}/"
+DEFAULT_FILE_STORAGE = "aview.core.backends.MediaRootS3BotoStorage"
+STATICFILES_STORAGE = "aview.core.backends.StaticRootS3BotoStorage"
