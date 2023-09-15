20430f53266346707ed0f22ffb48d23de29c3f8b
Smyja <akposlive59@gmail.com>
2023-09-15 22:39:26 +0100

    update

--- vuewe/wsgi.py
+++ vuewe/wsgi.py
@@ -12,4 +12,6 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vuewe.localset.dev')
 
-app = get_wsgi_application()
+
+application = get_wsgi_application()
+app = application
